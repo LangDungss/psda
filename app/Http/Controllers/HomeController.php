a325303cb08fc90fbcab511c2e6c9baf6c8ff76d
brocode29 <hallobacot577@gmail.com>
2024-11-10 04:13:07 +0700

    push

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,5 +35,8 @@ class HomeController extends Controller
     {
         return view('home.nextdanau');
     }
-}   
-    
+
+    public function layanankami(){
+        return view('home.layananpsda.layanankami');
+    }
+}
--- resources/views/home/landing_page.blade.php
+++ resources/views/home/landing_page.blade.php
@@ -94,106 +94,76 @@
     </header>
 
     <!-- Hero Section -->
-
-    <section class="bg-blue-600 text-white">
-      <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-2/6 lg:items-center">
+<section class="bg-blue-600 text-white h-s">
+    <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-2/6 lg:items-center">
         <div class="mx-auto text-center w-full">
-          <div class="grid grid-cols-1 gap-8 lg:grid-cols-3 lg:gap-8">
-            <!-- Left image -->
-            <div class="flex justify-center items-center">
-              <img src="{{ asset('images/sumsel.png') }}" class="h-60 object-contain">
-            </div>
+            <div class="grid grid-cols-1 gap-8 lg:grid-cols-3 lg:gap-8">
+                <!-- Left image -->
+                <div class="flex justify-center items-center">
+                    <img src="{{ asset('images/sumsel.png') }}" class="h-60 object-contain">
+                </div>
 
-            <!-- Center text and logo -->
-            <div class="flex flex-col justify-center items-center">
-              <h1 class="text-4xl font-bold mb-4">SELAMAT DATANG DI NOTA DINAS DIGITAL</h1>
-              <p class="text-2xl font-semibold mb-8">DINAS PSDA PROVINSI <br> SUMATERA SELATAN</p>
-              <img src="{{ asset('images/logoall2.png') }}" class="h-20 object-contain">
-            </div>
+                <!-- Center text and logo -->
+                <div class="flex flex-col justify-center items-center">
+                    <h1 class="text-4xl font-bold mb-4">SELAMAT DATANG DI NOTA DINAS DIGITAL</h1>
+                    <p class="text-2xl font-semibold mb-8">DINAS PSDA PROVINSI <br> SUMATERA SELATAN</p>
+                    <img src="{{ asset('images/logoall2.png') }}" class="h-20 object-contain">
+                </div>
 
-            <!-- Right image -->
-            <div class="flex justify-center items-center">
-              <img src="{{ asset('images/psda1.png') }}" class="h-60 object-contain">
+                <!-- Right image -->
+                <div class="flex justify-center items-center">
+                    <img src="{{ asset('images/psda1.png') }}" class="h-60 object-contain">
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </section>
+    </div>
+    <!-- Add a blur effect at the bottom of the hero section -->
+    <div class="absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-black-800 via-blue-600 to-transparent backdrop-blur-md"></div>
+</section>
 
 
 
-    <!-- Layanan Section -->
-    <section id="layanan" class="py-16 bg-gray-100 ">
-      <div class="container mx-auto text-center">
-        <h2 class="text-3xl font-bold mb-10">Layanan Kami</h2>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div class="bg-white p-6 shadow-lg rounded-3xl">
-            <img src="{{asset('images/irigasi.jpg')}}" alt="Layanan 1" class="mb-4 mx-auto">
-            <h3 class="text-xl text-black font-bold mb-2">Irigasi</h3>
-            <h3 class="text-black">sistem atau metode penyediaan air secara buatan untuk tanaman di lahan pertanian guna mendukung pertumbuhan tanaman, terutama di daerah yang kekurangan air atau pada musim kering..</h3>
-            <div class="flex justify-end gap-6">
-              <Button type="" class="rounded-full bg-black py-1 px-4 text-white">
-              <a href="/nextirigasi">lihat lebih lanjut </a>
-              </Button>
-            </div>
-          </div>
-          <div class="bg-white p-6 shadow-lg rounded-3xl">
-            <img src="{{asset('images/rawa.jpg')}}" alt="Layanan 2" class="mb-4 mx-auto">
-            <h3 class="text-xl font-bold mb-2">Rawa</h3>
-            <p>dataran rendah yang selalu tergenang air, baik secara permanen maupun musiman, dengan kedalaman air yang bervariasi. Rawa terbentuk karena air yang menggenang, baik itu air hujan, air sungai, atau air tanah, tidak dapat mengalir keluar dengan mudah akibat topografi atau kondisi geologis daerah tersebut.</p>
-            <div class="flex justify-end gap-6">
-              <Button type="" class="rounded-full bg-black py-1 px-4 text-white">
-              <a href="/nextrawa">lihat lebih lanjut </a>
-              </Button>
-            </div>
-          </div>
-          <div class="bg-white p-6 shadow-lg rounded-3xl">
-            <img src="{{asset('images/sungai.jpg')}}" alt="Layanan 3" class="mb-4 mx-auto">
-            <h3 class="text-xl font-bold mb-2">Sungai</h3>
-            <p>aliran air alami yang mengalir dari tempat yang lebih tinggi ke tempat yang lebih rendah, biasanya menuju laut, danau, atau sungai yang lebih besar.</p>
-            <div class="flex justify-end gap-6">
-              <Button type="" class="rounded-full bg-black py-1 px-4 text-white">
-              <a href="/nextsungai">lihat lebih lanjut </a>
-              </Button>
-          </div>
-        </div>
+
+    <!--
+  Heads up! 👋
+
+  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
+-->
+<!--MAIN CONTENT-->
+<section
+  class="relative bg-[url({{ asset('images/banner.jpg') }})] bg-cover bg-center bg-no-repeat"
+>
+<div
+class="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
+></div>
+
+  <div
+    class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8 text-center"
+  >
+    <div class="max-w-xl mx-auto text-center">
+      <h1 class="text-2xl font-extrabold sm:text-5xl">
+
+
+        <strong class="block font-extrabold text-yellow-300">Layanan Kami</strong>
+      </h1>
+
+      <p class="mt-4 max-w-lg sm:text-xl/relaxed">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
+        numquam ea!
+      </p>
+
+      <div class="mt-8 flex flex-wrap gap-4 justify-center">
+        <a
+          href="/layananpsda"
+          class="block w-full rounded bg-blue-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-amber-600 focus:outline-none focus:ring active:bg-amber-600 sm:w-auto"
+        >
+          SELENGKAPNYA
+        </a>
       </div>
-      <div class="container mx-auto text-center">
-        <h2 class="text-3xl font-bold mb-10"></h2>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div class="bg-white p-6 shadow-lg rounded-3xl">
-            <img src="{{asset('images/danau.jpg')}}" alt="Layanan 1" class="mb-4 mx-auto">
-            <h3 class="text-xl font-bold mb-2">Danau</h3>
-            <p>sistem atau metode penyediaan air secara buatan untuk tanaman di lahan pertanian guna mendukung pertumbuhan tanaman, terutama di daerah yang kekurangan air atau pada musim kering..</p>
-            <div class="flex justify-end gap-6">
-              <Button type="" class="rounded-full bg-black py-1 px-4 text-white">
-              <a href="/nextirigasi">lihat lebih lanjut </a>
-              </Button>
-          </div>
-          </div>
-          <div class="bg-white p-6 shadow-lg rounded-3xl">
-            <img src="{{asset('images/sumur bor.jpeg')}}" alt="Layanan 2" class="mb-4 mx-auto">
-            <h3 class="text-xl font-bold mb-2">Sumur Bor</h3>
-            <p>dataran rendah yang selalu tergenang air, baik secara permanen maupun musiman, dengan kedalaman air yang bervariasi. Rawa terbentuk karena air yang menggenang, baik itu air hujan, air sungai, atau air tanah, tidak dapat mengalir keluar dengan mudah akibat topografi atau kondisi geologis daerah tersebut.</p>
-            <div class="flex justify-end gap-6">
-              <Button type="" class="rounded-full bg-black py-1 px-4 text-white">
-              <a href="/nextirigasi">lihat lebih lanjut </a>
-              </Button>
-          </div>
-          </div>
-          <div class="bg-white p-6 shadow-lg rounded-3xl">
-            <img src="{{asset('images/gedung psda.jpeg')}}" alt="Layanan 3" class="mb-4 mx-auto">
-            <h3 class="text-xl font-bold mb-2">Pengelolaan Sumber Daya Air Lainnya</h3>
-            <p>aliran air alami yang mengalir dari tempat yang lebih tinggi ke tempat yang lebih rendah, biasanya menuju laut, danau, atau sungai yang lebih besar.</p>
-            <div class="flex justify-end gap-6">
-              <Button type="" class="rounded-full bg-black py-1 px-4 text-white">
-              <a href="/nextirigasi">lihat lebih lanjut </a>
-              </Button>
-          </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    </div>
+  </div>
+</section>
+
 
 
     <!-- Tentang Kami Section -->
